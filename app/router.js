b7e14689ccbfab40e5ef585d5e81aeae5d530149
Luiz Eduardo <dudu.christo@gmail.com>
2019-02-15 07:43:38 +0100

    Refactor backend phase 1

--- app/router.js
+++ app/router.js
@@ -8,10 +8,11 @@ router.get('/', (req, res) => {
 });
 
 //url analysis
-router.get('/analize/:url', async (req, res) => {
-    const url = req.params.url;    
-    let info = await pageInfoController.getPageInfo(url);
+router.get('/analize/:url', async (req, res) => {   
+    const url = req.params.url;       
+    const info = await pageInfoController.getPageInfo(url);
     res.setHeader('Content-Type', 'application/json; charset=UTF-8');
     res.status(200).json(info);
 });
+
 module.exports = router;
--- app/src/controllers/pageInfoController.js
+++ app/src/controllers/pageInfoController.js
@@ -1,101 +1,10 @@
-const cheerio = require('cheerio');
-const request = require('request');
+const pageInfoService = require('../services/pageInfoService')
 
-const getPageInfo = async (uri) => {
-    const url = uri;
-    let result;
-    
-    try {
-        result = await doGetRequest(url);      
-    } catch (err) {
-        return {"error": "Invalid or unreachable URL!"};
-    }
-
-    const $ = cheerio.load(result);
-    const links = getLinks($);
-
-    let responseObj = {
-        "htmlVersion": getHtmlVersion(result),
-        "title": getTitle($),
-        "headings": getHeadings($),
-        "hasLoginForm": hasLoginForm($),
-        "internalLinks": links.internal.length,
-        "externalLinks": links.external.length
-    };   
-    return responseObj;
-};
-
-const getHtmlVersion = (data) => {
-    if (data.includes('<!DOCTYPE html>') || result.includes('<!DOCTYPE HTML>')) { 
-        return 'HTML 5';
-    }
-    return htmlVersion = 'HTML <= 4';
-};
-
-const getTitle = ($) => {
-    return $("title").text();
-};
-
-const getHeadings = ($) => {
-    let headings = [];
-
-    headings.push($('h1').length);
-    headings.push($('h2').length);
-    headings.push($('h3').length);
-    headings.push($('h4').length);
-    headings.push($('h5').length);
-    headings.push($('h6').length);
-
-    return headings;
-};
-
-const hasLoginForm = ($) => {
-    let passwordInput = $("[type=password]");
-    let hasLoginForm = false;
-
-    if (passwordInput[0] &&
-        passwordInput[0].name === "input" &&
-        passwordInput[0].attribs.type === 'password') {
-
-        hasLoginForm = true;
-    }
-
-    return hasLoginForm;
-};
-
-const getLinks = ($) => {
-    let links = {
-        internal: [],
-        external: []
-    };
-
-    let pageLinks = $('a');
-
-    $(pageLinks).each(function (i, link) {
-        if (!($(link).attr('href')))
-            return;
-        if ($(link).attr('href').indexOf('http') >= 0) {
-            links.external.push($(link).attr('href'));
-        } else {
-            links.internal.push($(link).attr('href'));
-        }
-    });
-
-    return links;
-};
-
-const doGetRequest = (url) => {
-    return new Promise((resolve, reject) => {
-        request.get(url, (error, response, body) => {
-            if (error) {
-                reject(error);
-            } else {
-                resolve(body)
-            }
-        });
-    });
+const getPageInfo = (url) => {
+    console.log('--->', url);
+    return pageInfoService.getPageInformation(url);
 };
 
 module.exports = {
     getPageInfo
-};
+}
