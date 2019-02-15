f31cd30ac774d9a9923abff1048257fba512b10a
dechristo <dudu.christo@gmail.com>
2019-02-15 15:59:12 +0100

    Started controller unit test refactoring

--- tests/controllers/pageInformationController.spec.js
+++ tests/controllers/pageInformationController.spec.js
@@ -1,10 +1,17 @@
-var expect = require('chai').expect;
-var pageInfoController = require('../../app/src/controllers/pageInformationController');
-var http_mocks = require('sinon');
-var should = require('should');
+const expect = require('chai').expect;
+const sinon = require('sinon');
+
+const PageInfoController = require('../../app/src/controllers/pageInformationController');
+const PageInformationService = require('./../../app/src/services/pageInformationService');
 
 describe('Page Info Controller', () => {
 	it('GET /scrap/analize/:url should return json data for valid url', async () => {
+		sinon.stub(PageInformationService.prototype, 'getPageInformation')
+
+		let res = PageInfoController.getPageInformation();
+
+		expect(res).to.not.be.null;
 	});	
 });		
 
+//https://stackoverflow.com/questions/14487809/how-to-mock-request-and-response-in-nodejs-to-test-middleware-controllers
