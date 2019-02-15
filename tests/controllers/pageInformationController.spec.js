0b9afb97169d389f873991c2dfaf6d213a49623d
dechristo <dudu.christo@gmail.com>
2019-02-15 15:52:26 +0100

    Refactor unit tests for router

--- tests/controllers/pageInformationController.spec.js
+++ tests/controllers/pageInformationController.spec.js
@@ -4,24 +4,7 @@ var http_mocks = require('sinon');
 var should = require('should');
 
 describe('Page Info Controller', () => {
-	/*it('GET /scrap/analize/:url should return page info for valid url', async () => {
-		const url = 'https://www.bookdepository.com';
-		result = await pageInfoController.getPageInfo(url);
-		expect(result).to.not.be.null;		  			
-		expect(result).to.haveOwnProperty('htmlVersion');
-		expect(result).to.haveOwnProperty('title');
-		expect(result).to.haveOwnProperty('headings');
-		expect(result).to.haveOwnProperty('hasLoginForm');
-		expect(result).to.haveOwnProperty('internalLinks');
-		expect(result).to.haveOwnProperty('externalLinks');
+	it('GET /scrap/analize/:url should return json data for valid url', async () => {
 	});	
-	
-	it('GET /scrap/analize/:url should return error for invalid url', async () =>{
-		const url = 'https://www.bookdepositasdory.com';
-		result = await pageInfoController.getPageInfo(url);
-		expect(result).to.not.be.null;		  			
-		expect(result).to.haveOwnProperty('error');
-		expect(result.error).to.equal('Invalid or unreachable URL!');
-	});*/
 });		
 
--- tests/router.spec.js
+++ tests/router.spec.js
@@ -1,40 +1,69 @@
 const sinon = require('sinon');
-const assert = require('assert');
 const request = require('request');
-const server = require('./../app/server');
-const PageInformationController = require('../app/src/controllers/pageInformationController');
+const expect = require('chai').expect;
+describe("Router", function () {
+    let requestMock;
 
-describe("Router", function() {
-    
-    describe("GET /", () => {       
+    beforeEach(() => {
+        requestMock = sinon.stub(request, 'get');
+    })
+
+    afterEach(() => {
+        request.get.restore();
+    })
+
+    describe("GET /", () => {
         it("should respond with HTTP 200", (done) => {
-            request.get('http://localhost:2000', (req, res) => {
-                assert.equal(res.statusCode, 200);
+            requestMock.yields(null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'text/html; charset=UTF-8'
+                    }   
+                },   
+                null);
+
+            request.get('http://localhost:2000', (err, res, body) => {
+                expect(res.statusCode).to.equal(200);
+                expect(res.headers['content-type']).to.equal('text/html; charset=UTF-8');
                 done();
             });
-        });
+        });    
     });
 
-    describe("GET Analize", () => {       
-      it("should respond with HTTP 200 and json data", async () => {
-        const responseMock = {
-                htmlVersion: "HTML 5",
-                title: "Awesome!",  
-                headings: [1, 12, 24, 0, 0, 0 ],
-                hasLoginForm: false,
-                internalLinks: 3,
-                externalLinks: 10
+    describe("GET Analize", () => {
+        it("should respond with HTTP 200 and json data", (done) => {
+            const responseBodyMock = {
+                status: 'success',
+                data: {
+                    htmlVersion: "HTML 5",
+                    title: "Awesome!",
+                    headings: [1, 12, 24, 0, 0, 0],
+                    hasLoginForm: false,
+                    internalLinks: 3,
+                    externalLinks: 10
+                }
             }
-            sinon.stub(PageInformationController, 'getPageInformation')
-                .callsFake(() => responseMock);
 
-            const res = await PageInformationController.getPageInformation();
-            assert.equal(typeof(res), 'object');
-            assert.equal(res.htmlVersion, 'HTML 5');
-            assert.equal(res.title, 'Awesome!');
-            assert.equal(res.hasLoginForm, false);
-            assert.equal(res.internalLinks, 3);
-            assert.equal(res.externalLinks, 10);
+            requestMock.yields(null, {
+                statusCode: 200,
+                headers: {
+                    'content-type': 'application/json'
+                    }   
+                },   
+                JSON.stringify(responseBodyMock));
+
+            request.get('http://localhost:2000/analize/www.somesite.com', (err, res, body) => {
+                body = JSON.parse(body);
+                expect(body.data).to.be.an('object');
+                expect(body.data).to.have.all.keys('htmlVersion', 'headings',
+                    'title', 'hasLoginForm', 'internalLinks', 'externalLinks');
+                expect(body.data.htmlVersion).to.equal('HTML 5')
+                expect(body.data.title).to.equal('Awesome!');
+                expect(body.data.hasLoginForm).to.be.false;
+                expect(body.data.internalLinks).to.equal(3);
+                expect(body.data.externalLinks).to.equal(10);
+                done();
+            });
         });
     });
 });
